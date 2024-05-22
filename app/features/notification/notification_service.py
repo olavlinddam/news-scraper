032b788976196b567c6853629451d764fbe42797
Olav <olavlinddam@outlook.com>
2024-05-22 22:08:13 +0200

    finished push implementation

--- app/features/notification/notification_service.py
+++ app/features/notification/notification_service.py
@@ -1,45 +1,37 @@
-
-
 import json
 import logging
-from typing import Dict, List
+from typing import Any, Dict, List
 
 import requests
+
+from app.features.news.news_article import NewsArticle
 
 
 class NotificationService:
     def __init__(self) -> None:
         self.logger = logging.getLogger(__name__)
-        
-        
-    def push(self, documents, subscribers):
+
+    def push(self, documents: List[NewsArticle], subscribers: List[Dict[str, Any]]) -> None:
         if not documents:
             return
         if not subscribers:
             return
-        
+
         for subscriber in subscribers:
-            # Get the list of clubs the subscriber is interested in
-            subscribed_clubs = subscriber['subscribed_to']
-        
-            # Filter documents based on the clubs the subscriber is interested in
-            relevant_documents = [doc for doc in documents if doc['club'] in subscribed_clubs]
-        
+            clubs_subscribed_to = subscriber['subscribed_to']
+            relevant_documents = [doc for doc in documents if doc.club in clubs_subscribed_to]  # Use.club instead of ['club']
+
             # If there are no relevant documents for this subscriber, skip to the next subscriber
             if not relevant_documents:
                 continue
-        
-            # document_dtos = []
-            # for document in relevant_documents:
-                
-            
+
             self.logger.info(f"Sending {len(relevant_documents)} documents to {subscriber['url']}")
-            data = json.dumps(relevant_documents)
-        
+            data = json.dumps([doc.__dict__ for doc in relevant_documents])  # Convert NewsArticle objects to dictionaries
+
             # Send the documents to the subscriber's URL
             try:
                 response = requests.post(subscriber['url'], data=data, headers={'Content-Type': 'application/json'})
-                response.raise_for_status() # Raises an HTTPError if the response status code is 4xx or 5xx
+                response.raise_for_status()  # Raises an HTTPError if the response status code is 4xx or 5xx
                 print(f"Successfully sent {len(relevant_documents)} documents to {subscriber['url']}")
             except requests.exceptions.HTTPError as e:
                 print(f"HTTP Error: {e}")
@@ -49,6 +41,3 @@ class NotificationService:
                 print(f"Timeout Error: {e}")
             except requests.exceptions.RequestException as e:
                 print(f"Something went wrong: {e}")
-        
-        
-        
